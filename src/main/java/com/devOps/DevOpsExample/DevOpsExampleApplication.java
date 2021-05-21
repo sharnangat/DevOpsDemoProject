611eba4a7b6d0738823049a02f409696b4e326b1
sharnangat <sanjaysharnangat@gmail.com>
2021-05-21 18:25:08 +0530

    commingt file
    1. first Commit

---
+++ src/main/java/com/devOps/DevOpsExample/DevOpsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.devOps.DevOpsExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devOps/DevOpsExample/DevOpsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devOps.DevOpsExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
